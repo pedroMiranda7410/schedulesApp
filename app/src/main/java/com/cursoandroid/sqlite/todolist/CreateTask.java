670d6ea1b0a4b428bb93da20193f2b7c1fd26579
pedroMiranda7410 <pmiranda7410@gmail.com>
2019-02-15 23:43:13 -0200

    feito CRUD faltando o update function

--- app/src/main/java/com/cursoandroid/sqlite/todolist/CreateTask.java
+++ app/src/main/java/com/cursoandroid/sqlite/todolist/CreateTask.java
@@ -2,31 +2,117 @@ package com.cursoandroid.sqlite.todolist;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class CreateTask extends AppCompatActivity {
 
     private EditText textoTitle;
     private EditText textoDescription;
     private Button botaoCriar;
-    private SQLiteDatabase bancoDados;
+    //public MainActivity objetoMainActivity = new MainActivity();
+    public SQLiteDatabase bancoDados;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
-
+    try {
         //Recuperar Componentes
         textoTitle = (EditText) findViewById(R.id.titleText);
         textoDescription = (EditText) findViewById(R.id.descriptionText);
         botaoCriar = (Button) findViewById(R.id.botaoCriar);
+
+        bancoDados = openOrCreateDatabase("scheduleApp", MODE_PRIVATE, null);
+
+
+
+        /* EVENTO ON CLICK NO BOTAO SEM CRIAR FUNÇÃO
+
+         * botaoAdicionar.setOnClickListener(new View.OnClickListener() {
+         *
+         * @Override
+         * public void onClick(View v) {
+         *
+         *
+         * }
+         * });
+         *
+         * */
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void botaoCriarTask(View view){
+
+        String textoTitleDigitado = textoTitle.getText().toString();
+        String textoDescriptionDigitado = textoDescription.getText().toString();
+        AlertDialog alerta;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //define o titulo
+        builder.setTitle("Erro");
+        //define a mensagem
+        builder.setMessage("Preencha todos os campos para criar");
+
+        if(textoTitleDigitado.compareTo("") != 0) {
+            if (textoDescriptionDigitado.compareTo("") != 0) {
+                try {
+
+                    bancoDados.execSQL("INSERT INTO schedules(title,description) VALUES ('" + textoTitleDigitado + "', '" + textoDescriptionDigitado + "')");
+                    Toast.makeText(CreateTask.this, "Schedule saved successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent2 = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent2);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                //cria o AlertDialog
+                alerta = builder.create();
+                //Exibe
+                alerta.show();
+            }
+        }
+        else if(textoDescriptionDigitado.compareTo("") != 0){
+            if (textoTitleDigitado.compareTo("") != 0) {
+                try {
+
+                    bancoDados.execSQL("INSERT INTO schedules(title,description) VALUES ('" + textoTitleDigitado + "', '" + textoDescriptionDigitado + "')");
+                    Toast.makeText(CreateTask.this, "Schedule saved successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent2 = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent2);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                //cria o AlertDialog
+                alerta = builder.create();
+                //Exibe
+                alerta.show();
+            }
+        }
+        else{
+            //cria o AlertDialog
+            alerta = builder.create();
+            //Exibe
+            alerta.show();
+        }
+
+
+
+    }
+
+    public void botaoCancelarTask(View view){
 
         Intent intent2 = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent2);
--- app/src/main/java/com/cursoandroid/sqlite/todolist/MainActivity.java
+++ app/src/main/java/com/cursoandroid/sqlite/todolist/MainActivity.java
@@ -1,21 +1,59 @@
 package com.cursoandroid.sqlite.todolist;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private ListView listaSchedules;
+    public SQLiteDatabase bancoDados;
+    private ArrayAdapter<String> adaptadorSchedules;
+    private ArrayList<String> itens;
+    private ArrayList<Integer> ids;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try {
 
+            listaSchedules = (ListView) findViewById(R.id.listaSchedules);
+
+            //CRIAR BANCO
+            bancoDados = openOrCreateDatabase("scheduleApp", MODE_PRIVATE, null);
+
+            //Criar Tabelas
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS schedules(id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR, description VARCHAR)");
+
+            //Listar Schedules
+            recuperarTarefas();
+
+            //Deletar da lista
+            listaSchedules.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    removerSchedule(ids.get(position));
+                }
+            });
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void irParaTelaDeCriação(View view){
@@ -23,4 +61,57 @@ public class MainActivity extends AppCompatActivity {
         Intent intent1 = new Intent(getApplicationContext(), CreateTask.class);
         startActivity(intent1);
     }
+
+    private void recuperarTarefas(){
+
+        try{
+
+            Cursor cursorBanco = bancoDados.rawQuery("SELECT * FROM schedules ORDER BY id DESC", null);
+
+            //Pegar indices de cada coluna
+            int indiceColunaID = cursorBanco.getColumnIndex("id");
+            int indiceColunaTitle = cursorBanco.getColumnIndex("title");
+            int indiceColunaDescription = cursorBanco.getColumnIndex("description");
+
+            //Lista
+            listaSchedules = (ListView) findViewById(R.id.listaSchedules);
+
+            //Criando adaptador
+            itens = new ArrayList<String>();
+            adaptadorSchedules = new ArrayAdapter<String>(getApplicationContext(),
+                    android.R.layout.simple_selectable_list_item,
+                    android.R.id.text1,
+                    itens);
+            listaSchedules.setAdapter(adaptadorSchedules);
+
+            ids = new ArrayList<Integer>();
+
+
+            cursorBanco.moveToFirst();
+
+            while(cursorBanco != null){
+                Log.i("RESULTADO ID", cursorBanco.getString(indiceColunaID));
+                Log.i("RESULTADO Title", cursorBanco.getString(indiceColunaTitle));
+                Log.i("RESULTADO Description",cursorBanco.getString(indiceColunaDescription));
+                itens.add(cursorBanco.getString(indiceColunaTitle));
+                ids.add(Integer.parseInt(cursorBanco.getString(indiceColunaID)));
+                cursorBanco.moveToNext();
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void removerSchedule(Integer id){
+        try{
+
+            bancoDados.execSQL("DELETE FROM schedules WHERE id="+ id);
+            recuperarTarefas();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
